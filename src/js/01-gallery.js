ab111c4f74d692136d4b95015e3511f0191efe98
Luydmyla <lytic.ok@gmail.com>
2021-12-18 03:40:33 +0200

    add task -01
    add  simplelightbox
    add @vimeo/playe
    add lodash.throttle

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+// --- ----нашла ссылку на элемент div.gallery ------------
+const galleryListElem = document.querySelector('.gallery');
+console.log(galleryListElem);
+// --------------создаю разметку по шаблону (шаблонная строка)----------
+const listImageElement = createListImageElement(galleryItems);
+function createListImageElement(galleryItems) {
+
+     return galleryItems.map(galleryItem => ` <a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a> `).join("");
+}
+// console.log(listImageElement);
+
+// --------------рендер разметки (2 способа)-------------
+// galleryListElem.innerHTML = listImageElements;
+galleryListElem.insertAdjacentHTML("beforeend", listImageElement)
+
+// --------- использую библиотеку симпллайтбокс как обработчик событий по клику на изображения-----------------
+ let gallery = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    })
